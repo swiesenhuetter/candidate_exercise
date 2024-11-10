8d4cf716e811290b1ced3972b7ba5440d4edc059
Stephan Wiesenhütter <wiesenhuetter@gmail.com>
2024-11-10 10:18:41 +0100

    use zaber package for hardware access

---
+++ exercise1/zaber_axes.py
@@ -0,0 +1,35 @@
+from zaber_motion import Units
+from zaber_motion.ascii import Connection
+
+class ZaberAxes:
+    def __init__(self):
+        self.serial_port="COM3"
+
+        self.connection = Connection.open_serial_port(self.serial_port)
+        devices = self.connection.detect_devices()
+        if not devices:
+            raise Exception('No Zaber devices found')
+
+        self.controller = self.connection.detect_devices()[0]
+        self.controller_connected = True
+
+        self.left = self.controller.get_axis(1)
+        self.right = self.controller.get_axis(2)
+
+    def move_left(self, value):
+        self.left.move_absolute(value)
+
+    def move_y(self, value):
+        self.right.move_absolute(value)
+
+    def home(self):
+        self.controller.all_axes.home()
+
+    def get_positions(self: tuple[float, float]):
+        left_pos = self.left.get_position(Units.LENGTH_MILLIMETRES)
+        right_pos = self.left.get_position(Units.LENGTH_MILLIMETRES)
+        return (left_pos, right_pos)
+
+    def close(self):
+        if self.controller is not None:
+            self.connection.close()
